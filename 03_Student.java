06afd27b938b4acaee2f27916b99ef88b9944a95
PrkCSE1606-TechDEV <prkcsc1606@gmail.com>
2026-09-11 10:46:25 +0000

    this()Constructor

---
+++ 03_Student.java
@@ -0,0 +1,24 @@
+class Student 
+{
+    int a, b;
+    Student()
+    {
+        this(100);
+        a=20;
+        b=10;
+        System.out.println(a+b);
+    }
+    Student(int s)
+    {
+        this(15.5);
+        System.out.println(s);
+    }
+    Student(double d)
+    {
+        System.out.println(d);
+    }
+    public static void main(String[] args)
+    {
+        Student std = new Student();
+    }
+}
