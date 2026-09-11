b7ee0c011eb81d7f912ad711b228b63cbca30610
PrkCSE1606-TechDEV <prkcsc1606@gmail.com>
2026-09-11 10:29:52 +0000

    ConstructorDemo

---
+++ ConstructorDemo.java
@@ -0,0 +1,31 @@
+class ConstructorDemo
+{
+    int a, b, c;
+    double p;;
+    ConstructorDemo()
+    {
+        a=20;
+        b=0;
+        c=25;
+        p=17.5;
+        System.out.println("Inside Default Constructor");
+    }
+    ConstructorDemo(int x, int y)
+    {
+        a=x;
+        b=y;
+        c=a+b;
+        System.out.println("Inside Parameterized Constructor");
+        System.out.println(c);
+    }
+    public static void main(String[] args) 
+    { 
+        ConstructorDemo cd = new ConstructorDemo();
+        ConstructorDemo obj = new ConstructorDemo(5, 6);
+    }  
+}
+
+/* In this program in lines 23 and 24 there is yellow hilighting over cd and obj 
+   because they are declared but not used. cd and obj store the created objects, 
+   but nothing accesses them afterward. This is a warning, not a runtime or compilation failure.
+*/
