bd15092b3df42e7f4e7d1a7fe0db2ba9996f8ac7
PrkCSE1606-TechDEV <prkcsc1606@gmail.com>
2026-09-16 11:40:20 +0000

    Command Line

---
+++ CommandLine.java
@@ -0,0 +1,10 @@
+class CommandLine 
+{
+    public static void main (String ar[])
+    {
+        for (int i = 0; i < ar.length; i++) // Loop through the command line arguments
+        {
+            System.out.println(ar[i]);
+        }
+    }    
+}
